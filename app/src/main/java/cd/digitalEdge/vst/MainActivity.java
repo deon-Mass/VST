b51ce44375570a32fbd6da7789b59b1a088dc565
Deon_Massadi <gedeonmass15@gmail.com>
2020-06-12 13:27:34 +0200

    VST upload : 12/06/2020 13h 27

--- app/src/main/java/cd/digitalEdge/vst/MainActivity.java
+++ app/src/main/java/cd/digitalEdge/vst/MainActivity.java
@@ -3,25 +3,55 @@ package cd.digitalEdge.vst;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ActionBarContextView;
 import androidx.cardview.widget.CardView;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.multidex.MultiDex;
 
+import android.Manifest;
 import android.app.ActionBar;
 import android.app.AlertDialog;
+import android.app.SearchManager;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Cartesian;
+import com.anychart.charts.Cartesian3d;
+import com.anychart.charts.LinearGauge;
+import com.anychart.charts.Pie;
+import com.anychart.charts.Polar;
+import com.anychart.charts.Venn;
+import com.anychart.core.cartesian.series.Column;
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+import com.karumi.dexter.listener.multi.SnackbarOnAnyDeniedMultiplePermissionsListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import cd.digitalEdge.vst.Tools.Preferences;
 import cd.digitalEdge.vst.Views.Lists.List_clients;
@@ -34,6 +64,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     NavigationView navigationView;
     DrawerLayout drawer;
     CardView CARD_CLIENTS;
+    AnyChartView anyChartView;
+    SearchView searchView;
+    ProgressBar charts_progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +78,42 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         if (Preferences.getUserPreferences(context, "logged").equals("")){}
         INIT_COMPONENT();
+        conditions();
+        CheckPermission();
+        set_Charts();
+    }
+
+
+
+    private void conditions(){
+        if (getIntent().hasExtra("logged")){
+            Snackbar.make(drawer, (CharSequence) "Bienvenu (e)", 5000).show();
+        }
+    }
+
+    private void CheckPermission(){
+        Dexter.withContext(context)
+                .withPermissions(
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.CALL_PHONE,
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_NETWORK_STATE
+                )
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport multiplePermissionsReport) {
+                        if (multiplePermissionsReport.isAnyPermissionPermanentlyDenied() == true)
+                            ;//Toast.makeText(context, "Has granted", Toast.LENGTH_SHORT).show();
+                        else ;//Toast.makeText(context, "Has denied", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> list, PermissionToken permissionToken) {
+
+                    }
+                }).check();
+
     }
 
     private void INIT_COMPONENT(){
@@ -52,8 +121,32 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawer = findViewById(R.id.drawer);
         CARD_CLIENTS = findViewById(R.id.CARD_CLIENTS);
         navigationView = findViewById(R.id.navigationView);
+        charts_progressBar = findViewById(R.id.charts_progressBar);
+        anyChartView = (AnyChartView) findViewById(R.id.any_chart_view);
 
         navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    private void set_Charts(){
+        Pie pie = AnyChart.pie();
+        Cartesian c = AnyChart.bar();
+
+        List<DataEntry> data = new ArrayList<>();
+        data.add(new ValueDataEntry("Entrées", 17000));
+        data.add(new ValueDataEntry("Sorties", 12000));
+        data.add(new ValueDataEntry("Autre", 18000));
+
+        c.data(data);
+        anyChartView.setProgressBar(charts_progressBar);
+        anyChartView.setZoomEnabled(true);
+        anyChartView.onSaveInstanceState();
+        anyChartView.setChart(c);
+    }
+
+    public void Setting_badge(String count2) {
+        if (!count2.equals("0")) {
+
+        }
     }
 
     @Override
@@ -78,10 +171,18 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
         boolean m = true;
-        if (id == R.id.titre1) {
+        if (id == R.id.dr_project) {
             Toast.makeText(context, "test", Toast.LENGTH_SHORT).show();
             m = true;
-        } else if (id == R.id.titre1) {
+        } else if (id == R.id.dr_commerciaux) {
+            Toast.makeText(context, "test", Toast.LENGTH_SHORT).show();
+            m = true;
+        }else if (id == R.id.dr_client) {
+            Intent i  = new Intent(context, Login.class);
+            startActivity(i);
+            finish();
+            m = true;
+        }else if (id == R.id.dr_params) {
             Toast.makeText(context, "test", Toast.LENGTH_SHORT).show();
             m = true;
         }
@@ -95,20 +196,39 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
+
         getMenuInflater().inflate(R.menu.main_menu, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         super.onOptionsItemSelected(item);
         switch (item.getItemId()) {
+            case R.id.search :
+                SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
+                searchView = findViewById(R.id.search);
+                searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+                searchView.setIconifiedByDefault(false);
+                searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                    @Override
+                    public boolean onQueryTextSubmit(String query) {
+                        Toast.makeText(context, query, Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+
+                    @Override
+                    public boolean onQueryTextChange(String newText) {
+                        Toast.makeText(context, newText, Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
+                break;
             case R.id.profil :
-                drawer.openDrawer(GravityCompat.END );
+                if (drawer.isDrawerOpen(GravityCompat.END)) drawer.closeDrawer(GravityCompat.END);
+                else drawer.openDrawer(GravityCompat.END );
                 break;
             case R.id.logout :
-
                 new AlertDialog.Builder(context)
                         .setTitle("Exit")
                         .setMessage("Voulez-vous vraiment quitter l'application ?")
--- app/src/main/java/cd/digitalEdge/vst/Views/Signin/Login.java
+++ app/src/main/java/cd/digitalEdge/vst/Views/Signin/Login.java
@@ -4,13 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONArrayRequestListener;
+import com.google.android.material.snackbar.Snackbar;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import cd.digitalEdge.vst.Controllers.Config;
+import cd.digitalEdge.vst.Controllers.Online.Selects_queries;
 import cd.digitalEdge.vst.MainActivity;
+import cd.digitalEdge.vst.Objects.Users;
 import cd.digitalEdge.vst.R;
+import cd.digitalEdge.vst.Tools.Tool;
+import cd.digitalEdge.vst.Tools.Utils;
 import cd.digitalEdge.vst.Views.Lists.List_clients;
 
 public class Login extends AppCompatActivity {
@@ -18,6 +39,7 @@ public class Login extends AppCompatActivity {
     Context context = this;
     Button BTN_connection;
     ProgressBar logging_loading;
+    EditText username,passeword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +52,11 @@ public class Login extends AppCompatActivity {
 
     private void INIT_COMPONENT() {
         BTN_connection = findViewById(R.id.BTN_connection);
+        username = findViewById(R.id.username);
+        passeword = findViewById(R.id.passeword);
         logging_loading = findViewById(R.id.logging_loading);
 
+        logging_loading.setVisibility(View.GONE);
 
     }
 
@@ -41,12 +66,89 @@ public class Login extends AppCompatActivity {
         BTN_connection.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i  = new Intent(context, MainActivity.class);
-                startActivity(i);
-                finish();
+                String user_name = username.getText().toString();
+                String pass = passeword.getText().toString();
+                if (TextUtils.isEmpty(user_name)) {
+                    username.setError("Veuiillez saisir votre username");
+                } else if (TextUtils.isEmpty(pass)) {
+                    passeword.setError("Veuiillez saisir le mot de passe");
+                } else {
+                    NetworkInfo info = ((ConnectivityManager) context.getSystemService(CONNECTIVITY_SERVICE)).getActiveNetworkInfo();
+                    if (info == null || !info.isConnected()) {
+                        Snackbar.make(v, (CharSequence) "Vous n'êtes pas connecté", 5000).show();
+                    } else {
+                        if (user_name.equals("deon") && pass.equals("123456") ) {
+                            //auth(user_name, pass);
+                            Intent i  = new Intent(context, MainActivity.class);
+                            i.putExtra("logged", true);
+                            startActivity(i);
+                            finish();
+                        }else{
+                            Snackbar.make(v, (CharSequence) "Authentification incorrect", 5000).show();
+
+                        }
+                    }
+                }
+
+
             }
         });
     }
+
+
+
+    public void auth(String phone2, String passe2) {
+        logging_loading.setVisibility(View.VISIBLE);
+        String value = null;
+        try {
+            value = Selects_queries.auth(phone2, Tool.SHA1(passe2));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        AndroidNetworking.post(Config.SERVER_PATH)
+                .addBodyParameter("auth", Config.auth)
+                .addBodyParameter("method", "1")
+                .addBodyParameter("query", value)
+                .setPriority(Priority.HIGH)
+                .build()
+                .getAsJSONArray(new JSONArrayRequestListener() {
+                    public void onResponse(JSONArray response) {
+                        String str = "AN_ERROR1";
+                        Log.e(str, response.toString());
+                        String str2 = "Authentification";
+                        if (response == null || response.length() < 1) {
+                            Utils.MonToast(Login.this.context, str2, "Authentification incorrect", "Danger");
+                            logging_loading.setVisibility(View.GONE);
+                            return;
+                        }
+                        int i = 0;
+                        while (i < response.length()) {
+                            try {
+                                JSONObject jsonObject = response.getJSONObject(i);
+                                Users u = new Users();
+                                Users columns = new Users();
+                                u.setId(jsonObject.getString(columns.id));
+
+                                Tool.userPreferences_Set(Login.this.context, u);
+                                Intent intent = new Intent(Login.this.context, MainActivity.class);
+                                Utils.MonToast(Login.this.context, str2, "Authentification correct", "Success");
+                                Login.this.startActivity(intent);
+                                Login.this.finish();
+                                i++;
+                            } catch (Exception e) {
+                                Log.e(str, e.getMessage());
+                            }
+                        }
+                    }
+                    public void onError(ANError error) {
+                        logging_loading.setVisibility(View.GONE);
+                        Toast.makeText(context, error.toString(), Toast.LENGTH_LONG).show();
+                        Log.e("AN_ERROR", error.getMessage());
+                    }
+                });
+    }
+
 
 
 
